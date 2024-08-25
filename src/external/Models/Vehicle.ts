4d6becf1916dbf17ca087e45d8065cbf9952e6df
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2024-08-25 03:26:16 -0400

    limpar campos

--- src/external/Models/Vehicle.ts
+++ src/external/Models/Vehicle.ts
@@ -6,34 +6,64 @@ class Vehicle
   extends Model<InferAttributes<Vehicle>, InferCreationAttributes<Vehicle>>
   implements IVehicle {
   declare id: CreationOptional<number>
+  declare vehicle_type: string | null
+  declare id_model: number | null
   declare model: string | null
+  declare id_brand: number | null
   declare brand: string | null
-  declare year: number | null
+  declare year: string | null
   declare price: number | null
+  declare notes: string | null
+  declare km: number | null
+  declare color: string | null
 }
 
 Vehicle.init(
   {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, 
       autoIncrement: true,
       primaryKey: true,
       allowNull: false,
-    },
-    model: {
+    }, 
+    vehicle_type: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_brand: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     brand: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    year: {
+    id_model: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
     price: {
       type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    km: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
   },
--- src/external/interfaces/IVehicle.ts
+++ src/external/interfaces/IVehicle.ts
@@ -1,7 +1,13 @@
 export default interface IVehicle {
   id: number
+  vehicle_type: string | null
+  id_model: number | null
   model: string | null
+  id_brand: number | null
   brand: string | null
-  year: number | null
+  year: string | null
   price: number | null
+  notes: string | null
+  km: number | null
+  color: string | null
 }
